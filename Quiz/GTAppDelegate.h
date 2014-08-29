047b7227dc7488a36aa4848b3cb00a2aa5d69d8a
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-08-29 23:05:11 +0530

    Initial Commit

---
+++ Quiz/GTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GTAppDelegate.h
+//  Quiz
+//
+//  Created by Guwanjith Tennekoon on 8/29/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Guwanjith Tennekoon on 8/29/14.
+//  Copyright (c) 2014 GT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GTAppDelegate class]));
+    }
+}
